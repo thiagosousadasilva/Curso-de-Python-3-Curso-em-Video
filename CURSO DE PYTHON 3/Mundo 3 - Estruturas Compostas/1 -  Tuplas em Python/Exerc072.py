da13993f40081f76b5e81c16a4ccc99c27b904e4
Thiago Sousa <10331515+thiagosousadasilva@users.noreply.github.com>
2020-05-29 00:13:59 -0300

    Update Exerc072.py

--- CURSO DE PYTHON 3/Mundo 3 - Estruturas Compostas/1 -  Tuplas em Python/Exerc072.py
+++ CURSO DE PYTHON 3/Mundo 3 - Estruturas Compostas/1 -  Tuplas em Python/Exerc072.py
@@ -1,6 +1,11 @@
 ''' 
-Exercício Python 072: Crie um programa que tenha uma dupla totalmente preenchida 
+Exercício Python 072: Crie um programa que tenha uma tupla totalmente preenchida 
 com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número
 pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
 '''
 print("== Exercício Python #072 - Número por Extenso ==")
+
+num_extenso = ('zero','um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 
+'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+escolha = input('Digite um número entre 0 e 20: ')
+print(f'\033[1;34m{num_extenso[int(escolha)].capitalize()}\033[m')
