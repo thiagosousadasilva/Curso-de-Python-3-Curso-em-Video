38f569ee6fa90a86fabd9d83fd82888d0f0e92bb
Thiago Sousa <10331515+thiagosousadasilva@users.noreply.github.com>
2020-06-04 20:32:08 -0300

    Update Exerc095 - Aprimorando os Dicionários.py

--- CURSO DE PYTHON 3/Mundo 3 - Estruturas Compostas/3 - Dicionários em Python/Exerc095 - Aprimorando os Dicionários.py
+++ CURSO DE PYTHON 3/Mundo 3 - Estruturas Compostas/3 - Dicionários em Python/Exerc095 - Aprimorando os Dicionários.py
@@ -13,23 +13,31 @@ while True:
     for x in range(1, partidas+1):
         gol_part = int(input(f"{x}ª partida: "))
         gols.append(gol_part)
-        tot_gol += gol_part
-    jogadores.append({'nome': nome, 'part': partidas, 'gols': gols, 'tot_gol': tot_gol})
-    esc = input('Continuar [S/N]: ')
-    if esc in 'nN':
+    jogadores.append({'nome': nome, 'part': partidas, 'gols': gols, 'tot_gol': sum(gols)})
+    while True:
+        esc = input('Continuar [S/N]: ').upper()       
+        if esc in 'SN':
+            break
+        print("Digite apenas 'S' [sim] ou 'N' [não].")
+    if esc == 'N':
         break
     print('-'*40)
 print('-'*50)
 print(f"{'Relatório de jogadores':^50}")
 print('-'*50)
-print(f"Total de {len(jogadores)} jogadores cadastrados.")
+print('Cód. | Jogador')
+print('-'*25)
 for key, valor in enumerate(jogadores):
-    print(f"{key} - {valor['nome']}")
+    print(f"{key:^5}| {valor['nome']}")
+print('-'*25)    
+print(f"Total de {len(jogadores)} jogadores cadastrados.")
 while True:
     print('-'*50)    
     cod = int(input('Código do jogador p/ detalhes ou 999 para sair: '))
     if cod == 999:
         break
+    if cod >= len(jogadores):
+        print(f'ERRO! Não existe jogador com esse código!')
     else:
         print('-'*50)            
         print(f"- {jogadores[cod]['nome']} jogou {jogadores[cod]['part']} partidas.")
