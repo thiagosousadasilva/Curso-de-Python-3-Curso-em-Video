5ec4122050dcb0543c2811f2f55cb93b394b9ed6
Thiago Sousa <10331515+thiagosousadasilva@users.noreply.github.com>
2020-06-04 19:10:56 -0300

    Update Exerc094 - Unindo dicionários e listas.py

--- CURSO DE PYTHON 3/Mundo 3 - Estruturas Compostas/3 - Dicionários em Python/Exerc094 - Unindo dicionários e listas.py
+++ CURSO DE PYTHON 3/Mundo 3 - Estruturas Compostas/3 - Dicionários em Python/Exerc094 - Unindo dicionários e listas.py
@@ -9,3 +9,28 @@ C) Uma lista com as mulheres
 D) Uma lista de pessoas com idade acima da média
 '''
 print("== Exercício Python #094 - Unindo dicionários e listas: ==")
+pessoas = []
+while True:
+    nome = input('Nome: ')
+    sexo = input('Sexo [M/F]: ').upper()
+    idade = int(input('Idade: '))
+    pessoas.append({'nome': nome, 'sexo': sexo, 'idade': idade})
+    esc = input('Continuar [S/N]: ')
+    if esc in 'nN':
+        break
+print('-'*50)
+print(f"A) Foram cadastradas {len(pessoas)} pessoas.")
+total_idade = 0
+for x in pessoas:
+    total_idade += x['idade']
+media_idade = total_idade/len(pessoas)
+print(f"B) A média de idade: {media_idade:.0f} anos.")
+print("C) Mulheres cadastradas: ")
+for x in pessoas:
+    if x['sexo'] == 'F':
+        print('   -', x['nome'].title())
+print("D) Pessoas com idade acima da média: ")
+for x in pessoas:
+    if x['idade'] >= media_idade:
+        print('   -', x["nome"].title())
+print('-'*50)        
