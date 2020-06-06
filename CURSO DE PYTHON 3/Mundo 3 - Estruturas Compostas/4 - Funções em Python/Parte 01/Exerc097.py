4b5b691457a5f7e1185d56d21db7a52bdc30e5c4
Thiago Sousa <10331515+thiagosousadasilva@users.noreply.github.com>
2020-06-06 18:40:54 -0300

    Resolvido Exerc097

--- CURSO DE PYTHON 3/Mundo 3 - Estruturas Compostas/4 - Funções em Python/Parte 01/Exerc097.py
+++ CURSO DE PYTHON 3/Mundo 3 - Estruturas Compostas/4 - Funções em Python/Parte 01/Exerc097.py
@@ -4,3 +4,11 @@ Faça um programa que tenha uma função chamada escreva(),
 que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.
 '''
 print("== Exercício Python #097 - Um print especial ==")
+def escreva(x):
+    y = len(x) + 4
+    print('-'*y)
+    print(f'{x.title():^{y}}')
+    print('-'*y)
+
+texto = input('Digite o texto para ser formatado: ')
+escreva(texto)
