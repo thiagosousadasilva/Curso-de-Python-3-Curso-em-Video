eecb88e0e9aa77473f14c0a33a26ea754baa581d
Thiago Sousa <10331515+thiagosousadasilva@users.noreply.github.com>
2020-06-15 11:48:36 -0300

    Resolvido exerc104

---
+++ CURSO DE PYTHON 3/Mundo 3 - Estruturas Compostas/4 - Funções em Python/Parte 02/Exerc104 - Validando entrada de dados em Python.py
@@ -0,0 +1,20 @@
+''' 
+Exercício Python #104 - Validando entrada de dados em Python:
+Crie um programa que tenha a função leiaInt(), que vai funcionar de forma
+semelhante à função input() do Python, só que fazendo a validação para 
+aceitar apenas um valor numérico. 
+Ex: n = leiaInt('Digite um n: ')
+'''
+print("== Exercício Python #104 - Validando entrada de dados em Python ==")
+def leia_int(texto):
+    while True:
+        num = input(texto)
+        if num.isnumeric():
+            return int(num)
+            break
+        else:
+            print('\033[1;31mDigite um número inteiro válido.\033[m')
+
+# Programa principal
+n = leia_int('Digite um número: ')
+print(f'Você digitou o número {n}')
